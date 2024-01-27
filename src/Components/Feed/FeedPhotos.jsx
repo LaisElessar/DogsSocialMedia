2f596b1f38948fe36e11bd62682aba6fbff9af55
LaisElessar <laisreisdejesus45@gmail.com>
2024-01-27 13:32:32 -0400

    Adds feed modal

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -6,7 +6,7 @@ import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = () => {
+const FeedPhotos = ({setModalPhoto}) => {
 
     const {data, loading, error, request} = useFetch();
 
@@ -24,7 +24,7 @@ const FeedPhotos = () => {
       return (
         <ul className={`${styles.feed} animeLeft`}>
           {data.map((photo) => (
-          <FeedPhotosItem key={photo.id} photo={photo}/>))}
+          <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto}/>))}
         </ul>
       );
       else return null;
