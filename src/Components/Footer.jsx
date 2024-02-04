a404a0055f256adebabf1e6d2b7b537c84aca8fa
LaisElessar <laisreisdejesus45@gmail.com>
2024-02-04 18:42:47 -0400

    Adds infinited feed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,6 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <footer>
-      Footer
     </footer>
   );
 }
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -4,13 +4,17 @@ import { Route, Routes } from 'react-router-dom';
 import Feed from '../Feed/Feed';
 import UserPhotoPost from './UserPhotoPost';
 import UserStatistic from './UserStatistic';
+import { UserContext } from '../../UserContext';
 
 const User = () => {
+
+  const { data }= React.useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader/>
       <Routes>
-        <Route path='/' element={<Feed/>}/>
+        <Route path='/' element={<Feed user={data.id}/>}/>
         <Route path='postar' element={<UserPhotoPost/>}/>
         <Route path='estatisticas' element={<UserStatistic/>}/>
       </Routes>
